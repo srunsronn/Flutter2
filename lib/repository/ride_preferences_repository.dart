a9161e0a3ae14fafd49cf4ccab6d21650b5300d8
srunsronn <158978515+srunsronn@users.noreply.github.com>
2025-03-06 15:52:48 +0700

    Delete lib directory

--- lib/repository/ride_preferences_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../model/ride_pref/ride_pref.dart';
-
-abstract class RidePreferencesRepository {
-
-  List<RidePreference> getPastPreferences();
-
-  void addPreference(RidePreference preference);
-}
